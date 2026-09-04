7c6114f5684e3ea81db7c66f4f0418bcb4120ffa
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-04 23:47:01 +0530

    feat(user): add login controller, login router and input validation for login

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -19,6 +20,45 @@ export const register = async (req: Request, res: Response) => {
     res
       .status(201)
       .json({ success: true, message: "User created successfully" });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
+    res.status(500).json({ success: false, message: errorMessage });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("password");
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User doesn't exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect email or password" });
+    }
+
+    if (!process.env.JWT_SECRET) return;
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ success: true, message: "Logged in successfully" });
   } catch (error) {
     const errorMessage =
       error instanceof Error ? error.message : "An unknown error occurred";
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import { login, register } from "../controllers/userController.js";
 import { validate } from "../middlewares/validate.js";
-import { registerSchema } from "../validators/userValidator.js";
+import { loginSchema, registerSchema } from "../validators/userValidator.js";
 
 const router = express.Router();
 
 router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
 
 export default router;
