270e1301e01912631f95afd47dc929a69987e74d
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-05 12:21:00 +0530

    feat(chat): add chat model

---
+++ server/src/models/chatModel.ts
@@ -0,0 +1,20 @@
+import mongoose, { Document, Model } from "mongoose";
+
+interface IChat extends Document {
+  user: mongoose.Types.ObjectId;
+  title: string;
+}
+
+const chatSchema = new mongoose.Schema<IChat>({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: { type: String, required: true },
+});
+
+const Chat: Model<IChat> =
+  mongoose.models.Chat || mongoose.model<IChat>("Chat", chatSchema);
+
+export default Chat;
