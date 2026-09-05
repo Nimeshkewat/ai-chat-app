b8255aacb6ad04050e1f87d7c4c8d8aa0106580c
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-05 12:21:45 +0530

    feat(chat): add create chat, get chats, get single chat and delete chat controller functions

---
+++ server/src/controllers/chatController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import Chat from "../models/chatModel.js";
+
+export const createChat = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.user;
+    const { title } = req.body;
+
+    const chat = await Chat.create({
+      user: id,
+      title: title || "New Chat",
+    });
+
+    res.status(201).json({ success: true, chat });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknow error occirred";
+    res.status(500).json({ success: false, message: errorMessage });
+  }
+};
+
+export const getChats = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.user;
+
+    const chats = await Chat.find({ user: id }).lean();
+    if (chats.length === 0) {
+      return res.status(404).json({ success: false, message: "No chat found" });
+    }
+
+    res.status(200).json({ success: true, chats });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknow error occirred";
+    res.status(500).json({ success: false, message: errorMessage });
+  }
+};
+
+export const getChat = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.user;
+    const { chatId } = req.params;
+
+    const chat = await Chat.findOne({ user: id, _id: chatId }).lean();
+    if (!chat) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Chat not found" });
+    }
+
+    res.status(200).json({ success: true, chat });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknow error occirred";
+    res.status(500).json({ success: false, message: errorMessage });
+  }
+};
+
+export const deleteChat = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.user;
+    const { chatId } = req.params;
+
+    const chat = await Chat.findOneAndDelete({ user: id, _id: chatId });
+    if (!chat) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Chat not found" });
+    }
+
+    res.status(200).json({ success: true, chat });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknow error occirred";
+    res.status(500).json({ success: false, message: errorMessage });
+  }
+};
