7424b9dfecd08c51586813e19023ae7ec1bb41b0
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-06 14:11:40 +0530

    feat(auth): add public and private protected routes

---
+++ client/src/components/protected-routes/ProtectedRoutes.tsx
@@ -0,0 +1,23 @@
+import { useAuth } from "@/context/AuthContext";
+import type { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
+
+export const PrivateRoute = ({ children }: { children: ReactNode }) => {
+  const { isAuthenticated, isLoading } = useAuth();
+  if (isLoading) return <h2>Loading...</h2>;
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
+export const PublicRoute = ({ children }: { children: ReactNode }) => {
+  const { isAuthenticated, isLoading } = useAuth();
+  if (isLoading) return <h2>Loading...</h2>;
+
+  if (isAuthenticated) {
+    return <Navigate to="/chat" replace />;
+  }
+  return children;
+};
