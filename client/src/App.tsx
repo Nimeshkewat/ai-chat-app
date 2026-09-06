dca3ff88e52d73e365f6d2b62e328472327e3bae
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-06 14:14:33 +0530

    Define routes, configure react-query and wrap application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,50 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+import {
+  PrivateRoute,
+  PublicRoute,
+} from "./components/protected-routes/ProtectedRoutes";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <Home />
+            </PublicRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          element={
+            <PublicRoute>
+              <AuthLayout />
+            </PublicRoute>
+          }
+        >
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./context/AuthContext.tsx";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const client = new QueryClient();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={client}>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
