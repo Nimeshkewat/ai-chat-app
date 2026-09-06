52ae393768b11b433036cdea1d7271bfdea9cdca
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-06 14:04:33 +0530

    feat(auth): create AuthContext and Provider using useCheckAuth

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,28 @@
+import { useCheckAuth } from "../hooks/auth/useCheckAuth";
+import { createContext, useContext, type ReactNode } from "react";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+function AuthContextProvider({ children }: { children: ReactNode }) {
+  const { data, isLoading } = useCheckAuth();
+  const isAuthenticated = data?.user ? true : false;
+
+  if (isLoading) return <h1>Loading...</h1>;
+
+  const value = { isAuthenticated, isLoading };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used inside AuthProvider");
+  }
+  return context;
+};
+
+export default AuthContextProvider;
