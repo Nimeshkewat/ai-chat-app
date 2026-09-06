d414024d9c7d8c80e4156b8ab064fdbde805452e
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-06 13:57:53 +0530

    feat(user): mutation for user sign-out and query for fetching current user session

---
+++ client/src/hooks/auth/useCheckAuth.ts
@@ -0,0 +1,17 @@
+import type { CheckAuthResponse } from "@/types/auth";
+import { api } from "../../api";
+import { useQuery } from "@tanstack/react-query";
+import type { ApiError } from "@/types/axios";
+
+const checkAuth = async (): Promise<CheckAuthResponse> => {
+  const response = await api.get("/users/check-auth");
+  return response.data;
+};
+
+export const useCheckAuth = () => {
+  return useQuery<CheckAuthResponse, ApiError>({
+    queryKey: ["check-auth"],
+    queryFn: checkAuth,
+    retry: false,
+  });
+};
---
+++ client/src/hooks/auth/useLogout.ts
@@ -0,0 +1,15 @@
+import { api } from "@/api";
+import type { LogoutResponse } from "@/types/auth";
+import type { ApiError } from "@/types/axios";
+import { useMutation } from "@tanstack/react-query";
+
+const logout = async (): Promise<LogoutResponse> => {
+  const response = await api.post("/users/logout");
+  return response.data;
+};
+
+export const useLogout = () => {
+  return useMutation<LogoutResponse, ApiError, undefined>({
+    mutationFn: logout,
+  });
+};
