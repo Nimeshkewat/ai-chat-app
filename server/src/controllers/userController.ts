3aa90288176551d703cceba32c36c2545ba2d672
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-07 12:19:40 +0530

    feat(auth): remove max age option from in logout controller

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -72,7 +72,6 @@ export const logout = async (req: Request, res: Response) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "productio" ? "none" : "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.status(200).json({ success: true, message: "Logged out successfully" });
