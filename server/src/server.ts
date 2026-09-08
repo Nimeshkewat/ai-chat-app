1cfb76f5c10f2f3705c8e467d185668189cba9d7
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-08 11:51:17 +0530

    Add cloudinary config and resuable image upload function

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import app from "./app.js";
+import { connectCloudinary } from "./config/cloudinary.js";
 import connectDb from "./config/db.js";
 
 const port = process.env.PORT || 4000;
+
 connectDb();
+connectCloudinary();
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
