975389945bcdc62229fcd0595b4e6b34a17023ed
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-10 13:06:00 +0530

    fix: remove log

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -34,7 +34,6 @@ function Register() {
       return setApiError("Password and confirm password do not match");
     }
     setApiError("");
-    setInputErrors({});
 
     const result = registerSchema.safeParse(input);
     if (!result.success) {
@@ -51,8 +50,7 @@ function Register() {
     setApiError("");
 
     register(input, {
-      onSuccess: async (data) => {
-        console.log(data);
+      onSuccess: async () => {
         navigate("/login");
       },
       onError: (error) => {
